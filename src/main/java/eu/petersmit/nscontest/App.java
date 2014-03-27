219d3646d0f0fd2290a380b9b28ca2667c328a38
Peter Smit <peter@smitmail.eu>
2014-03-27 22:30:14 +0200

    Add command line options

--- src/main/java/eu/petersmit/nscontest/App.java
+++ src/main/java/eu/petersmit/nscontest/App.java
@@ -1,12 +1,9 @@
 package eu.petersmit.nscontest;
 
-import au.com.bytecode.opencsv.CSVReader;
-import au.com.bytecode.opencsv.CSVWriter;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.apache.commons.cli.*;
+
+import java.io.*;
 
 /**
  * Hello world!
@@ -16,7 +13,39 @@ public class App
 {
     public static void main( String[] args )
     {
+        Options options = new Options();
+        options.addOption("h", "help", false, "print this message");
+        options.addOption("d", "input-directory", true, "directory to find the input csv files. Default: Current working directory");
+        options.addOption("o", "output", true, "output-file. If not given or '-', output to terminal (std out)");
+
+        CommandLineParser parser = new BasicParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse( options, args );
+            if (line.hasOption("help")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("nscontest", options);
+                return;
+            }
+
+            String directory = ".";
+            if (line.hasOption('d')) directory = line.getOptionValue('d');
+
+            Writer w = new PrintWriter(System.out);
+            if (line.hasOption('o') && !line.getOptionValue('o').equals("-")) {
+                w = new FileWriter(line.getOptionValue('o'));
+            }
+
+        }
+        catch( ParseException exp ) {
+            // oops, something went wrong
+            System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
+
+
 }
